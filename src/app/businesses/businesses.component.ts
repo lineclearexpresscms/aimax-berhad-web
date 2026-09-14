7f8a36d3a4e9096330bac31469f76841e64442c1
JieLing <jielingcsh@gmail.com>
2026-09-14 10:35:01 +0800

    initial commit 3

--- src/app/businesses/businesses.component.ts
+++ src/app/businesses/businesses.component.ts
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
 })
 export class BusinessesComponent {
   divisions = [
-    { slug: 'gloves', title: 'Gloves & NBL Manufacturing', tagline: 'Becoming one of the notable glove makers in the nation', img: 'assets/images/Nitrile Glove.png', link: '/businesses/gloves/nitrile' },
+    { slug: 'gloves', title: 'Gloves & NBL Manufacturing', tagline: 'Becoming one of the notable glove makers in the nation', img: 'assets/images/Nitrile Glove.png', link: '/businesses/manufacturing' },
     { slug: 'healthcare', title: 'Healthcare', tagline: 'End-to-end integrated healthcare supply chain specialist', img: 'assets/images/HEALTHCARE.png', link: '/businesses/healthcare' },
     { slug: 'finance', title: 'Financial Services', tagline: 'Delivering capable & reliable business solutions', img: 'assets/images/FINANCIAL.png', link: '/businesses/financial-services' }
   ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeComponent {
   divisions = [
-    { slug: 'gloves', title: 'Gloves & NBL Manufacturing', tagline: 'Becoming one of the notable glove makers in the nation', img: 'assets/images/HOMEPAGE-01.png', link: '/businesses/gloves/nitrile' },
+    { slug: 'gloves', title: 'Gloves & NBL Manufacturing', tagline: 'Becoming one of the notable glove makers in the nation', img: 'assets/images/HOMEPAGE-01.png', link: '/businesses/manufacturing' },
     { slug: 'healthcare', title: 'Healthcare', tagline: 'End-to-end integrated healthcare supply chain specialist', img: 'assets/images/HOMEPAGE-02.png', link: '/businesses/healthcare' },
     {slug:'finance',title:'Financial Services',tagline:'Delivering Capable & Reliable Business Financing Solutions',img:'assets/images/HOMEPAGE-03.png',link:'/businesses/financial-services'}
   ];
